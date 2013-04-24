ab9d23ddbf4ac75dc0972b19ef2ec6bfbc1a0db5
ulrix lihtenshtein <ulihtenshtein@gmail.com>
2013-04-24 18:23:02 +0700

    half done not work

--- sven d 5.js
+++ sven d 5.js
@@ -3,14 +3,128 @@
 //if its class is called Robot
 var Robot = function(robot) {
     robot.clone();
+    Robot.doner = DefaultDoner.getInstance();
+    Robot.sdoner = DefaultOnScannedRobotDoner.getInstance();
    
-    //robot.ignore('onScannedRobot');
-    //logObj(robot, robot);
-    //robot.rotateCannon(360);
-    //logPos(robot);
 };
 Robot.to = 0;
 Robot.toClone = 1;
+
+
+Robot.prototype.onIdle = function(ev) {
+  Robot.doner.doIt(ev);
+};
+
+Robot.prototype.onScannedRobot = function(ev) {
+  var robot = ev.robot;
+  
+  if( (ev.scannedRobot.parentId == robot.id) || 
+     (ev.scannedRobot.id == robot.parentId) ) {
+    return;
+  };
+  checkOnScanned(ev);
+  Robot.sdoner.doIt(ev);
+   
+};
+
+Robot.prototype.onHitByBullet = function(ev) {
+  var robot = ev.robot;
+  //alert(ev.bearing);
+  robot.disappear();
+  
+  //robot.rotateCannon(ev.bearing);
+  robot.fire();
+};
+
+Robot.prototype.onWallCollision = function(ev) {
+  var  r = ev.robot;
+
+  if( r.parentId == null ) {
+      Robot.to = Robot.to < 3 ? Robot.to + 1 : 0; 
+  } else {
+    Robot.toClone = Robot.toClone < 3 ? Robot.toClone + 1 : 0; 
+  };
+
+  robot.trun(ev.bearing +15);
+};
+
+Robot.prototype.onRobotCollision = function(ev) {
+  var r = ev.robot;
+
+  if( r.parentId == null ) {
+      Robot.to = Robot.to < 3 ? Robot.to + 1 : 0; 
+  } else {
+    Robot.toClone = Robot.toClone < 3 ? Robot.toClone + 1 : 0; 
+  };
+
+  r.trun(ev.bearing + 20);
+};
+
+//default strategy.....................................
+function DefaultDoner() {
+ this.doIt = function(ev) {
+   var r = ev.robot;
+  var  amount = 100 + Math.random()*50;
+  var who =  r.parentId ? Robot.toClone : Robot.to;
+  switch( who ) {
+    case 0 :
+       toUp(amount);
+       break;
+    case 1:
+       toRight(amount);
+       break;
+    case 2:
+       toDown(amount);
+       break;
+    case 3:
+       toLeft(amount);
+      break;
+    default: 
+  };
+
+   r.rotateCannon(45);
+   r.rotateCannon(-90);
+   r.rotateCannon(45);
+
+   var direct = Math.random()*4^0;
+   r.parentId ? Robot.toClone = direct : Robot.to = direct;
+   // r.parentId && Robot.to = direct || Robot.toClone = direct;
+   //Robot.to = direct;//Robot.to < 3 ? Robot.to + 1 : 0
+ };
+};
+
+DefaultDoner.getInstance = function () {
+  if ( DefaultDoner.obj === undefined ) {
+    DefaultDoner.obj = new DefaultDoner();
+  };
+     return DefaultDoner.obj;
+ };
+
+//default on scannedRobot .................
+function  DefaultOnScannedRobotDoner() {
+
+   this.doIt = function(ev) {
+   var cln = 1;
+   var cln = robot.parentId ? 1 : -1;
+  
+    robot.rotateCannon(cln*2);
+    robot.fire();
+    robot.ahead(cln*10);
+    robot.turn(cln*15);
+    robot.rotateCannon(cln*(-20));
+ };
+};
+
+DefaultOnScannedRobotDoner.getInstance = function() {
+  if( DefaultOnScannedRobotDoner.obj === undefined ) {
+    DefaultOnScannedRobotDoner.obj = new DefaultOnScannedRobotDoner();
+  };
+    return DefaultOnScannedRobotDoner.obj;
+};
+
+
+function checkOnScanned(ev) {
+};
 function logObj(obj, robot) {
   for( var keys in obj) {
     robot.log( '' + keys + ':' + obj[keys] +'\n');
@@ -44,33 +158,6 @@ function toUp(amount) {
 function logPos(robot) {
    robot.log('(' + robot.position.x + ',' + robot.position.y + ')');
 };
-Robot.prototype.onIdle = function(ev) {
-  Robot.ev = ev;
-   var r = ev.robot;
-  var  amount = 100 + Math.random()*50;
-  var who =  r.parentId ? Robot.toClone : Robot.to;
-  switch( who ) {
-    case 0 :
-       toUp(amount);
-       break;
-    case 1:
-       toRight(amount);
-       break;
-    case 2:
-       toDown(amount);
-       break;
-    case 3:
-       toLeft(amount);
-      break;
-    default: 
-  };
-   r.rotateCannon(45);
-   r.rotateCannon(-90);
-   r.rotateCannon(45);
-   var direct = Math.random()*4^0;
-  r.parentId? Robot.toClone = direct: Robot.to = direct;
-   //Robot.to = direct;//Robot.to < 3 ? Robot.to + 1 : 0;
-};
 function cloneIdle(robot){
    logPos(robot);
    robot.rotateCannon(360);
@@ -78,55 +165,4 @@ function cloneIdle(robot){
     robot.turn(30);
     robot.ahead(150);
 };
-Robot.prototype.onScannedRobot = function(ev) {
-  var robot = ev.robot;
-  var cln = 1;
- 
-  
-  var cln = robot.parentId ? 1 : -1;
-  if( (ev.scannedRobot.parentId == robot.id) || (ev.scannedRobot.id == robot.parentId) ) {
-    return;
-  }; /* 
-  if( ev.scannedRobot.parentId ) { 
-    //return;
-  };
-  */
-    //robot.fire(); 
-    robot.rotateCannon(cln*2);
-    robot.fire();
-    robot.ahead(cln*10);
-    robot.turn(cln*15);
-    robot.rotateCannon(cln*(-20));
-    //robot.ahead(cln*15);
-    //robot.rotateCannon(-20);
-  //robot.fire();
-};
-Robot.prototype.onHitByBullet = function(ev) {
-  var robot = ev.robot;
-  //alert(ev.bearing);
-  robot.disappear();
-  
-  //robot.rotateCannon(ev.bearing);
-  robot.fire();
-};
-Robot.prototype.onWallCollision = function(ev) {
-  var  r = ev.robot;
-  var  p = r.position;
-  if( r.parentId == null ) {
-      Robot.to = Robot.to < 3 ? Robot.to + 1 : 0; 
-  } else {
-    Robot.toClone = Robot.toClone < 3 ? Robot.toClone + 1 : 0; 
-  }
-  robot.trun(ev.bearing +15);//
-  /*
-  var robot = ev.robot;
-  robot.trun(90 - ev.bearing);
-  robot.back(50);
-  */
-};
-Robot.prototype.onRobotCollision = function(ev) {
-  var robot = ev.robot;
-  Robot.to = Robot.to < 3 ? Robot.to + 1 : 0;
-  robot.trun(ev.bearing + 20);
- // robot.back(30);
-};
+
